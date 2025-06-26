e706abca437bc30beec45b6ab29dfb8c44f09458
Jeeyoung HAN <jeeyoung.han82@gmail.com>
2025-06-26 14:54:45 +0900

    remove console

--- ios/RNScreenshotDetector/RNScreenshotDetector.m
+++ ios/RNScreenshotDetector/RNScreenshotDetector.m
@@ -18,12 +18,10 @@
 RCT_EXPORT_MODULE();
 
 - (NSArray<NSString *> *)supportedEvents {
-    NSLog(@"[RNScreenshotDetector] supportedEvents called");
     return @[@"ScreenshotTaken", @"ScreenRecordingChanged"];
 }
 
 - (void)startObserving {
-    NSLog(@"[RNScreenshotDetector] startObserving called");
     NSOperationQueue *mainQueue = [NSOperationQueue mainQueue];
     
     if (screenshotObserver == nil) {
@@ -34,9 +32,6 @@ RCT_EXPORT_MODULE();
             usingBlock:^(NSNotification *notification) {
                 [self screenshotDetected:notification];
             }];
-        NSLog(@"[RNScreenshotDetector] Screenshot observer registered");
-    } else {
-        NSLog(@"[RNScreenshotDetector] Screenshot observer already exists");
     }
     
     if (screenRecordingObserver == nil) {
@@ -47,63 +42,45 @@ RCT_EXPORT_MODULE();
             usingBlock:^(NSNotification *notification) {
                 [self screenRecordingChanged:notification];
             }];
-        NSLog(@"[RNScreenshotDetector] Screen recording observer registered");
-    } else {
-        NSLog(@"[RNScreenshotDetector] Screen recording observer already exists");
     }
 }
 
 - (void)stopObserving {
-    NSLog(@"[RNScreenshotDetector] stopObserving called");
     if (screenshotObserver != nil) {
         [[NSNotificationCenter defaultCenter] removeObserver:screenshotObserver];
         screenshotObserver = nil;
-        NSLog(@"[RNScreenshotDetector] Screenshot observer removed");
     }
     
     if (screenRecordingObserver != nil) {
         [[NSNotificationCenter defaultCenter] removeObserver:screenRecordingObserver];
         screenRecordingObserver = nil;
-        NSLog(@"[RNScreenshotDetector] Screen recording observer removed");
     }
 }
 
 - (void)screenshotDetected:(NSNotification *)notification {
-    NSLog(@"[RNScreenshotDetector] Screenshot detected!");
     if (screenshotObserver != nil) {
-        NSLog(@"[RNScreenshotDetector] Sending ScreenshotTaken event");
         [self sendEventWithName:@"ScreenshotTaken" body:@{}];
-    } else {
-        NSLog(@"[RNScreenshotDetector] Screenshot observer is nil, not sending event");
     }
 }
 
 - (void)screenRecordingChanged:(NSNotification *)notification {
     BOOL isRecording = [UIScreen mainScreen].isCaptured;
-    NSLog(@"[RNScreenshotDetector] Screen recording changed: %@", isRecording ? @"YES" : @"NO");
     
     if (screenRecordingObserver != nil) {
-        NSLog(@"[RNScreenshotDetector] Sending ScreenRecordingChanged event");
         [self sendEventWithName:@"ScreenRecordingChanged" body:@{@"isRecording": @(isRecording)}];
-    } else {
-        NSLog(@"[RNScreenshotDetector] Screen recording observer is nil, not sending event");
     }
 }
 
 RCT_EXPORT_METHOD(disableScreenshots) {
-    NSLog(@"[RNScreenshotDetector] disableScreenshots called");
     dispatch_async(dispatch_get_main_queue(), ^{
         isProtectionEnabled = YES;
-        NSLog(@"[RNScreenshotDetector] Protection enabled, calling enableTrueScreenshotPrevention");
         [self enableTrueScreenshotPrevention];
     });
 }
 
 RCT_EXPORT_METHOD(enableScreenshots) {
-    NSLog(@"[RNScreenshotDetector] enableScreenshots called");
     dispatch_async(dispatch_get_main_queue(), ^{
         isProtectionEnabled = NO;
-        NSLog(@"[RNScreenshotDetector] Protection disabled, calling disableTrueScreenshotPrevention");
         [self disableTrueScreenshotPrevention];
     });
 }
@@ -111,42 +88,35 @@ RCT_EXPORT_METHOD(enableScreenshots) {
 RCT_EXPORT_METHOD(isScreenRecording:(RCTPromiseResolveBlock)resolve
                   rejecter:(RCTPromiseRejectBlock)reject) {
     BOOL isRecording = [UIScreen mainScreen].isCaptured;
-    NSLog(@"[RNScreenshotDetector] isScreenRecording called, result: %@", isRecording ? @"YES" : @"NO");
     resolve(@(isRecording));
 }
 
 RCT_EXPORT_METHOD(subscribeToScreenRecording) {
-    NSLog(@"[RNScreenshotDetector] subscribeToScreenRecording called (NOTE: activates both screenshot and screen recording observers)");
     [self startObserving];
 }
 
 RCT_EXPORT_METHOD(unsubscribeFromScreenRecording) {
-    NSLog(@"[RNScreenshotDetector] unsubscribeFromScreenRecording called");
     [self stopObserving];
 }
 
+// 감사자 지적 반영: 더 명확한 메서드명
 RCT_EXPORT_METHOD(subscribeToScreenshotAndScreenRecording) {
-    NSLog(@"[RNScreenshotDetector] subscribeToScreenshotAndScreenRecording called (clearer method name)");
     [self startObserving];
 }
 
 RCT_EXPORT_METHOD(unsubscribeFromScreenshotAndScreenRecording) {
-    NSLog(@"[RNScreenshotDetector] unsubscribeFromScreenshotAndScreenRecording called (clearer method name)");
     [self stopObserving];
 }
 
 // Screenshot Prevention using Secure Text Field
 - (void)enableTrueScreenshotPrevention {
-    NSLog(@"[RNScreenshotDetector] enableTrueScreenshotPrevention called");
     if (self.secureTextField == nil) {
-        NSLog(@"[RNScreenshotDetector] Creating new secureTextField");
         self.secureTextField = [[UITextField alloc] init];
         self.secureTextField.userInteractionEnabled = NO;
         self.secureTextField.secureTextEntry = YES;
         
         UIWindow *keyWindow = [self getKeyWindow];
         if (keyWindow != nil) {
-            NSLog(@"[RNScreenshotDetector] Key window found, setting up secure text field");
             [keyWindow makeKeyAndVisible];
             
             // Make the app window a sublayer of the secure text field
@@ -156,44 +126,30 @@ RCT_EXPORT_METHOD(unsubscribeFromScreenshotAndScreenRecording) {
             NSArray *sublayers = self.secureTextField.layer.sublayers;
             if (sublayers.count > 0) {
                 [sublayers.firstObject addSublayer:keyWindow.layer];
-                NSLog(@"[RNScreenshotDetector] Secure text field layers configured");
-            } else {
-                NSLog(@"[RNScreenshotDetector] No sublayers found in secure text field");
             }
-        } else {
-            NSLog(@"[RNScreenshotDetector] Key window not found!");
         }
     } else {
-        NSLog(@"[RNScreenshotDetector] Secure text field already exists, enabling secureTextEntry");
         self.secureTextField.secureTextEntry = YES;
     }
 }
 
 - (void)disableTrueScreenshotPrevention {
-    NSLog(@"[RNScreenshotDetector] disableTrueScreenshotPrevention called");
     if (self.secureTextField != nil) {
-        NSLog(@"[RNScreenshotDetector] Disabling secureTextEntry");
         self.secureTextField.secureTextEntry = NO;
-    } else {
-        NSLog(@"[RNScreenshotDetector] Secure text field is nil");
     }
 }
 
 - (UIWindow *)getKeyWindow {
-    NSLog(@"[RNScreenshotDetector] getKeyWindow called");
     UIWindow *keyWindow = nil;
     
     NSSet<UIScene *> *connectedScenes = [UIApplication sharedApplication].connectedScenes;
-    NSLog(@"[RNScreenshotDetector] Connected scenes count: %lu", (unsigned long)connectedScenes.count);
     
     for (UIScene *scene in connectedScenes) {
         if ([scene isKindOfClass:[UIWindowScene class]]) {
             UIWindowScene *windowScene = (UIWindowScene *)scene;
-            NSLog(@"[RNScreenshotDetector] Found window scene with %lu windows", (unsigned long)windowScene.windows.count);
             for (UIWindow *window in windowScene.windows) {
                 if (window.isKeyWindow) {
                     keyWindow = window;
-                    NSLog(@"[RNScreenshotDetector] Found key window using UIScene method");
                     break;
                 }
             }
@@ -201,15 +157,10 @@ RCT_EXPORT_METHOD(unsubscribeFromScreenshotAndScreenRecording) {
         }
     }
     
-    if (keyWindow == nil) {
-        NSLog(@"[RNScreenshotDetector] ERROR: Key window not found using UIScene method (iOS 16.0+)!");
-    }
-    
     return keyWindow;
 }
 
 - (void)dealloc {
-    NSLog(@"[RNScreenshotDetector] dealloc called");
     [self stopObserving];
     [self disableTrueScreenshotPrevention];
 }
