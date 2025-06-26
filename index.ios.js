1080aaf04fc681aad4770b9efa37b155289f0fff
Jeeyoung HAN <jeeyoung.han82@gmail.com>
2025-06-26 14:43:04 +0900

    wip

--- index.ios.js
+++ index.ios.js
@@ -1,52 +1,141 @@
 import { NativeEventEmitter, NativeModules } from 'react-native'
 
 const { RNScreenshotDetector } = NativeModules
+
+console.log('[RNScreenshotDetector] Native module loaded:', !!RNScreenshotDetector)
+console.log('[RNScreenshotDetector] Available methods:', Object.keys(RNScreenshotDetector || {}))
+
 const eventEmitter = new NativeEventEmitter(RNScreenshotDetector)
 
 const SCREENSHOT_EVENT = 'ScreenshotTaken'
 const SCREEN_RECORDING_EVENT = 'ScreenRecordingChanged'
 
 const subscribe = (cb) => {
+  console.log('[RNScreenshotDetector] subscribe called - FIXED: Now calling native observer as per audit feedback!')
+  
+  // ✅ 감사자 지적 반영: subscribe 메서드에서도 네이티브 observer 시작
+  if (RNScreenshotDetector && RNScreenshotDetector.subscribeToScreenRecording) {
+    console.log('[RNScreenshotDetector] CALLING native subscribeToScreenRecording from subscribe method')
+    try {
+      RNScreenshotDetector.subscribeToScreenRecording()
+      console.log('[RNScreenshotDetector] SUCCESS: subscribeToScreenRecording called from subscribe')
+    } catch (error) {
+      console.log('[RNScreenshotDetector] ERROR calling subscribeToScreenRecording:', error)
+    }
+  } else {
+    console.log('[RNScreenshotDetector] subscribeToScreenRecording method not available in subscribe')
+  }
+  
   const sub = eventEmitter.addListener(SCREENSHOT_EVENT, (data) => {
+    console.log('[RNScreenshotDetector] ScreenshotTaken event received:', data)
     cb(data)
   })
   return () => {
+    console.log('[RNScreenshotDetector] Unsubscribing from screenshot events')
+    
+    // ✅ 감사자 지적 반영: cleanup에서도 네이티브 observer 정리
+    if (RNScreenshotDetector && RNScreenshotDetector.unsubscribeFromScreenRecording) {
+      console.log('[RNScreenshotDetector] CALLING native unsubscribeFromScreenRecording from subscribe cleanup')
+      try {
+        RNScreenshotDetector.unsubscribeFromScreenRecording()
+        console.log('[RNScreenshotDetector] SUCCESS: unsubscribeFromScreenRecording called from subscribe cleanup')
+      } catch (error) {
+        console.log('[RNScreenshotDetector] ERROR calling unsubscribeFromScreenRecording:', error)
+      }
+    }
+    
     sub.remove()
   }
 }
 
 const subscribeToScreenRecording = (cb) => {
-  if (RNScreenshotDetector.subscribeToScreenRecording) {
-    RNScreenshotDetector.subscribeToScreenRecording()
+  console.log('[RNScreenshotDetector] subscribeToScreenRecording called')
+  console.log('[RNScreenshotDetector] RNScreenshotDetector exists:', !!RNScreenshotDetector)
+  console.log('[RNScreenshotDetector] subscribeToScreenRecording method exists:', !!(RNScreenshotDetector && RNScreenshotDetector.subscribeToScreenRecording))
+  
+  if (RNScreenshotDetector && RNScreenshotDetector.subscribeToScreenRecording) {
+    console.log('[RNScreenshotDetector] Calling native subscribeToScreenRecording')
+    try {
+      RNScreenshotDetector.subscribeToScreenRecording()
+      console.log('[RNScreenshotDetector] SUCCESS: Native subscribeToScreenRecording called')
+    } catch (error) {
+      console.log('[RNScreenshotDetector] ERROR calling native subscribeToScreenRecording:', error)
+    }
+  } else {
+    console.log('[RNScreenshotDetector] Native subscribeToScreenRecording method not available')
   }
   
   const sub = eventEmitter.addListener(SCREEN_RECORDING_EVENT, (data) => {
+    console.log('[RNScreenshotDetector] ScreenRecordingChanged event received:', data)
     cb(data)
   })
   return () => {
+    console.log('[RNScreenshotDetector] Unsubscribing from screen recording events')
     sub.remove()
-    if (RNScreenshotDetector.unsubscribeFromScreenRecording) {
-      RNScreenshotDetector.unsubscribeFromScreenRecording()
+    if (RNScreenshotDetector && RNScreenshotDetector.unsubscribeFromScreenRecording) {
+      console.log('[RNScreenshotDetector] Calling native unsubscribeFromScreenRecording')
+      try {
+        RNScreenshotDetector.unsubscribeFromScreenRecording()
+        console.log('[RNScreenshotDetector] SUCCESS: Native unsubscribeFromScreenRecording called')
+      } catch (error) {
+        console.log('[RNScreenshotDetector] ERROR calling native unsubscribeFromScreenRecording:', error)
+      }
     }
   }
 }
 
 const disableScreenshots = () => {
-  if (RNScreenshotDetector.disableScreenshots) {
-    RNScreenshotDetector.disableScreenshots()
+  console.log('[RNScreenshotDetector] disableScreenshots called')
+  console.log('[RNScreenshotDetector] RNScreenshotDetector exists:', !!RNScreenshotDetector)
+  console.log('[RNScreenshotDetector] disableScreenshots method exists:', !!(RNScreenshotDetector && RNScreenshotDetector.disableScreenshots))
+  
+  if (RNScreenshotDetector && RNScreenshotDetector.disableScreenshots) {
+    console.log('[RNScreenshotDetector] Calling native disableScreenshots')
+    try {
+      RNScreenshotDetector.disableScreenshots()
+      console.log('[RNScreenshotDetector] SUCCESS: Native disableScreenshots called')
+    } catch (error) {
+      console.log('[RNScreenshotDetector] ERROR calling native disableScreenshots:', error)
+    }
+  } else {
+    console.log('[RNScreenshotDetector] Native disableScreenshots method not available')
   }
 }
 
 const enableScreenshots = () => {
-  if (RNScreenshotDetector.enableScreenshots) {
-    RNScreenshotDetector.enableScreenshots()
+  console.log('[RNScreenshotDetector] enableScreenshots called')
+  console.log('[RNScreenshotDetector] RNScreenshotDetector exists:', !!RNScreenshotDetector)
+  console.log('[RNScreenshotDetector] enableScreenshots method exists:', !!(RNScreenshotDetector && RNScreenshotDetector.enableScreenshots))
+  
+  if (RNScreenshotDetector && RNScreenshotDetector.enableScreenshots) {
+    console.log('[RNScreenshotDetector] Calling native enableScreenshots')
+    try {
+      RNScreenshotDetector.enableScreenshots()
+      console.log('[RNScreenshotDetector] SUCCESS: Native enableScreenshots called')
+    } catch (error) {
+      console.log('[RNScreenshotDetector] ERROR calling native enableScreenshots:', error)
+    }
+  } else {
+    console.log('[RNScreenshotDetector] Native enableScreenshots method not available')
   }
 }
 
 const isScreenRecording = async () => {
-  if (RNScreenshotDetector.isScreenRecording) {
-    return RNScreenshotDetector.isScreenRecording()
+  console.log('[RNScreenshotDetector] isScreenRecording called')
+  console.log('[RNScreenshotDetector] RNScreenshotDetector exists:', !!RNScreenshotDetector)
+  console.log('[RNScreenshotDetector] isScreenRecording method exists:', !!(RNScreenshotDetector && RNScreenshotDetector.isScreenRecording))
+  
+  if (RNScreenshotDetector && RNScreenshotDetector.isScreenRecording) {
+    try {
+      const result = await RNScreenshotDetector.isScreenRecording()
+      console.log('[RNScreenshotDetector] isScreenRecording result:', result)
+      return result
+    } catch (error) {
+      console.log('[RNScreenshotDetector] ERROR calling isScreenRecording:', error)
+      return false
+    }
   } else {
+    console.log('[RNScreenshotDetector] Native isScreenRecording method not available')
     return false
   }
 }
--- ios/RNScreenshotDetector/RNScreenshotDetector.m
+++ ios/RNScreenshotDetector/RNScreenshotDetector.m
@@ -12,71 +12,98 @@
 {
     id screenshotObserver;
     id screenRecordingObserver;
-    UIView *securityOverlay;
     BOOL isProtectionEnabled;
 }
 
 RCT_EXPORT_MODULE();
 
 - (NSArray<NSString *> *)supportedEvents {
+    NSLog(@"[RNScreenshotDetector] supportedEvents called");
     return @[@"ScreenshotTaken", @"ScreenRecordingChanged"];
 }
 
 - (void)startObserving {
+    NSLog(@"[RNScreenshotDetector] startObserving called");
     NSOperationQueue *mainQueue = [NSOperationQueue mainQueue];
-    screenshotObserver = [[NSNotificationCenter defaultCenter] 
-        addObserverForName:UIApplicationUserDidTakeScreenshotNotification
-        object:nil
-        queue:mainQueue
-        usingBlock:^(NSNotification *notification) {
-            [self screenshotDetected:notification];
-        }];
     
-    screenRecordingObserver = [[NSNotificationCenter defaultCenter]
-        addObserverForName:UIScreenCapturedDidChangeNotification
-        object:nil
-        queue:mainQueue
-        usingBlock:^(NSNotification *notification) {
-            [self screenRecordingChanged:notification];
-        }];
+    if (screenshotObserver == nil) {
+        screenshotObserver = [[NSNotificationCenter defaultCenter] 
+            addObserverForName:UIApplicationUserDidTakeScreenshotNotification
+            object:nil
+            queue:mainQueue
+            usingBlock:^(NSNotification *notification) {
+                [self screenshotDetected:notification];
+            }];
+        NSLog(@"[RNScreenshotDetector] Screenshot observer registered");
+    } else {
+        NSLog(@"[RNScreenshotDetector] Screenshot observer already exists");
+    }
+    
+    if (screenRecordingObserver == nil) {
+        screenRecordingObserver = [[NSNotificationCenter defaultCenter]
+            addObserverForName:UIScreenCapturedDidChangeNotification
+            object:nil
+            queue:mainQueue
+            usingBlock:^(NSNotification *notification) {
+                [self screenRecordingChanged:notification];
+            }];
+        NSLog(@"[RNScreenshotDetector] Screen recording observer registered");
+    } else {
+        NSLog(@"[RNScreenshotDetector] Screen recording observer already exists");
+    }
 }
 
 - (void)stopObserving {
+    NSLog(@"[RNScreenshotDetector] stopObserving called");
     if (screenshotObserver != nil) {
         [[NSNotificationCenter defaultCenter] removeObserver:screenshotObserver];
         screenshotObserver = nil;
+        NSLog(@"[RNScreenshotDetector] Screenshot observer removed");
     }
     
     if (screenRecordingObserver != nil) {
         [[NSNotificationCenter defaultCenter] removeObserver:screenRecordingObserver];
         screenRecordingObserver = nil;
+        NSLog(@"[RNScreenshotDetector] Screen recording observer removed");
     }
 }
 
 - (void)screenshotDetected:(NSNotification *)notification {
+    NSLog(@"[RNScreenshotDetector] Screenshot detected!");
     if (screenshotObserver != nil) {
+        NSLog(@"[RNScreenshotDetector] Sending ScreenshotTaken event");
         [self sendEventWithName:@"ScreenshotTaken" body:@{}];
+    } else {
+        NSLog(@"[RNScreenshotDetector] Screenshot observer is nil, not sending event");
     }
 }
 
 - (void)screenRecordingChanged:(NSNotification *)notification {
+    BOOL isRecording = [UIScreen mainScreen].isCaptured;
+    NSLog(@"[RNScreenshotDetector] Screen recording changed: %@", isRecording ? @"YES" : @"NO");
+    
     if (screenRecordingObserver != nil) {
-        BOOL isRecording = [UIScreen mainScreen].isCaptured;
-        
+        NSLog(@"[RNScreenshotDetector] Sending ScreenRecordingChanged event");
         [self sendEventWithName:@"ScreenRecordingChanged" body:@{@"isRecording": @(isRecording)}];
+    } else {
+        NSLog(@"[RNScreenshotDetector] Screen recording observer is nil, not sending event");
     }
 }
 
 RCT_EXPORT_METHOD(disableScreenshots) {
+    NSLog(@"[RNScreenshotDetector] disableScreenshots called");
     dispatch_async(dispatch_get_main_queue(), ^{
         isProtectionEnabled = YES;
+        NSLog(@"[RNScreenshotDetector] Protection enabled, calling enableTrueScreenshotPrevention");
         [self enableTrueScreenshotPrevention];
     });
 }
 
 RCT_EXPORT_METHOD(enableScreenshots) {
+    NSLog(@"[RNScreenshotDetector] enableScreenshots called");
     dispatch_async(dispatch_get_main_queue(), ^{
         isProtectionEnabled = NO;
+        NSLog(@"[RNScreenshotDetector] Protection disabled, calling disableTrueScreenshotPrevention");
         [self disableTrueScreenshotPrevention];
     });
 }
@@ -84,26 +111,42 @@ RCT_EXPORT_METHOD(enableScreenshots) {
 RCT_EXPORT_METHOD(isScreenRecording:(RCTPromiseResolveBlock)resolve
                   rejecter:(RCTPromiseRejectBlock)reject) {
     BOOL isRecording = [UIScreen mainScreen].isCaptured;
+    NSLog(@"[RNScreenshotDetector] isScreenRecording called, result: %@", isRecording ? @"YES" : @"NO");
     resolve(@(isRecording));
 }
 
 RCT_EXPORT_METHOD(subscribeToScreenRecording) {
+    NSLog(@"[RNScreenshotDetector] subscribeToScreenRecording called (NOTE: activates both screenshot and screen recording observers)");
     [self startObserving];
 }
 
 RCT_EXPORT_METHOD(unsubscribeFromScreenRecording) {
+    NSLog(@"[RNScreenshotDetector] unsubscribeFromScreenRecording called");
+    [self stopObserving];
+}
+
+RCT_EXPORT_METHOD(subscribeToScreenshotAndScreenRecording) {
+    NSLog(@"[RNScreenshotDetector] subscribeToScreenshotAndScreenRecording called (clearer method name)");
+    [self startObserving];
+}
+
+RCT_EXPORT_METHOD(unsubscribeFromScreenshotAndScreenRecording) {
+    NSLog(@"[RNScreenshotDetector] unsubscribeFromScreenshotAndScreenRecording called (clearer method name)");
     [self stopObserving];
 }
 
 // Screenshot Prevention using Secure Text Field
 - (void)enableTrueScreenshotPrevention {
+    NSLog(@"[RNScreenshotDetector] enableTrueScreenshotPrevention called");
     if (self.secureTextField == nil) {
+        NSLog(@"[RNScreenshotDetector] Creating new secureTextField");
         self.secureTextField = [[UITextField alloc] init];
         self.secureTextField.userInteractionEnabled = NO;
         self.secureTextField.secureTextEntry = YES;
         
         UIWindow *keyWindow = [self getKeyWindow];
         if (keyWindow != nil) {
+            NSLog(@"[RNScreenshotDetector] Key window found, setting up secure text field");
             [keyWindow makeKeyAndVisible];
             
             // Make the app window a sublayer of the secure text field
@@ -113,29 +156,44 @@ RCT_EXPORT_METHOD(unsubscribeFromScreenRecording) {
             NSArray *sublayers = self.secureTextField.layer.sublayers;
             if (sublayers.count > 0) {
                 [sublayers.firstObject addSublayer:keyWindow.layer];
+                NSLog(@"[RNScreenshotDetector] Secure text field layers configured");
+            } else {
+                NSLog(@"[RNScreenshotDetector] No sublayers found in secure text field");
             }
+        } else {
+            NSLog(@"[RNScreenshotDetector] Key window not found!");
         }
     } else {
+        NSLog(@"[RNScreenshotDetector] Secure text field already exists, enabling secureTextEntry");
         self.secureTextField.secureTextEntry = YES;
     }
 }
 
 - (void)disableTrueScreenshotPrevention {
+    NSLog(@"[RNScreenshotDetector] disableTrueScreenshotPrevention called");
     if (self.secureTextField != nil) {
+        NSLog(@"[RNScreenshotDetector] Disabling secureTextEntry");
         self.secureTextField.secureTextEntry = NO;
+    } else {
+        NSLog(@"[RNScreenshotDetector] Secure text field is nil");
     }
 }
 
 - (UIWindow *)getKeyWindow {
+    NSLog(@"[RNScreenshotDetector] getKeyWindow called");
     UIWindow *keyWindow = nil;
     
     NSSet<UIScene *> *connectedScenes = [UIApplication sharedApplication].connectedScenes;
+    NSLog(@"[RNScreenshotDetector] Connected scenes count: %lu", (unsigned long)connectedScenes.count);
+    
     for (UIScene *scene in connectedScenes) {
         if ([scene isKindOfClass:[UIWindowScene class]]) {
             UIWindowScene *windowScene = (UIWindowScene *)scene;
+            NSLog(@"[RNScreenshotDetector] Found window scene with %lu windows", (unsigned long)windowScene.windows.count);
             for (UIWindow *window in windowScene.windows) {
                 if (window.isKeyWindow) {
                     keyWindow = window;
+                    NSLog(@"[RNScreenshotDetector] Found key window using UIScene method");
                     break;
                 }
             }
@@ -144,21 +202,14 @@ RCT_EXPORT_METHOD(unsubscribeFromScreenRecording) {
     }
     
     if (keyWindow == nil) {
-        keyWindow = [UIApplication sharedApplication].keyWindow;
-        if (keyWindow == nil) {
-            for (UIWindow *window in [UIApplication sharedApplication].windows) {
-                if (window.isKeyWindow) {
-                    keyWindow = window;
-                    break;
-                }
-            }
-        }
+        NSLog(@"[RNScreenshotDetector] ERROR: Key window not found using UIScene method (iOS 16.0+)!");
     }
     
     return keyWindow;
 }
 
 - (void)dealloc {
+    NSLog(@"[RNScreenshotDetector] dealloc called");
     [self stopObserving];
     [self disableTrueScreenshotPrevention];
 }
